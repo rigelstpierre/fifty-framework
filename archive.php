41f7ebefcb019999e8e154ee786bd7d1051cc47f
bryanmonzon <monzonb@gmail.com>
2013-09-13 17:21:26 -0700

    copied folder of changes to here

--- archive.php
+++ archive.php
@@ -1,9 +1,39 @@
 <?php get_header(); ?>
 
-	<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
 
-	Archive Template
+<div id="main" class="default blog">
+	<div class="container">
 
-	<?php endwhile; endif; ?>
+		<div class="sidebar push-<?php sidebar_position_class(); ?>">
+			<?php get_sidebar(); ?>
+		</div><!-- #sidebar -->
+
+
+		<div id="content" class="push-<?php sidebar_position_class(); ?>">
+			<div class="content-inner">
+				<h1 class="archive-title"><?php
+					if ( is_day() ) :
+						printf( __( 'Daily Archives: %s', 'twentythirteen' ), get_the_date() );
+					elseif ( is_month() ) :
+						printf( __( 'Monthly Archives: %s', 'twentythirteen' ), get_the_date( _x( 'F Y', 'monthly archives date format', 'twentythirteen' ) ) );
+					elseif ( is_year() ) :
+						printf( __( 'Yearly Archives: %s', 'twentythirteen' ), get_the_date( _x( 'Y', 'yearly archives date format', 'twentythirteen' ) ) );
+					else :
+						_e( 'Archives', 'twentythirteen' );
+					endif;
+				?></h1>
+				<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'loop/loop', get_post_format() ); ?>
+
+				<?php endwhile; endif; ?>
+
+				<?php do_action('FFW_pagination', array('id' => 'nav-below') ); ?>
+			</div>
+		</div><!-- .content -->
+
+
+	</div>
+</div>
 
 <?php get_footer(); ?>
--- page.php
+++ page.php
@@ -1,14 +1,27 @@
 <?php get_header(); ?>
 
-<div class="container">
-	<div class="content">
-	<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
-		<h1><?php the_title(); ?></h1>
-		<?php the_content(); ?>
-	<?php endwhile; endif; ?>
-	</div>
+<div id="main" class="default blog">
+	<div class="container">
 
-	<?php get_sidebar(); ?>
+		<div class="sidebar push-<?php sidebar_position_class(); ?>">
+			<?php get_sidebar(); ?>
+		</div><!-- #sidebar -->
+
+
+		<div id="content" class="push-<?php sidebar_position_class(); ?>">
+			<div class="content-inner">
+				
+				<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'loop/loop', get_post_format() ); ?>
+
+				<?php endwhile; endif; ?>
+
+			</div>
+		</div><!-- .content -->
+
+
+	</div>
 </div>
 
 <?php get_footer(); ?>
