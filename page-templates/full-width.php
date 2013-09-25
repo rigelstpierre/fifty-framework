faae2d7c8f2024162e441ece471b07be87f08e76
bryanmonzon <monzonb@gmail.com>
2013-09-25 12:57:29 -0700

    testing new full width

---
+++ page-templates/full-width.php
@@ -0,0 +1,26 @@
+<?php 
+/**
+* Template Name: Full Width
+*/
+get_header(); ?>
+
+<?php do_action('FFW_hero_before'); ?>
+<?php do_action('FFW_hero'); ?>
+<?php do_action('FFW_hero_after'); ?>
+
+<div id="main" class="page page-default default">
+  <div class="container">
+
+    <div id="content" class="full-wdith">
+      <div class="content-inner">
+        
+        <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+          <?php get_template_part( 'loop/loop', get_post_format() ); ?>
+        <?php endwhile; endif; ?>
+
+      </div><!-- .content-inner -->
+    </div><!-- #content -->
+  </div><!-- .container -->
+</div><!-- #main -->
+
+<?php get_footer(); ?>
