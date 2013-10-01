3ae970ba36482a9aba38f3b3a312ef0a0f6f5492
Alexander Zizzo <elzi@nerdi.net>
2013-10-01 16:26:41 -0700

    dear lord

--- index.php
+++ index.php
@@ -11,10 +11,10 @@
 
 
 	<div style="background-color:#999;width:100%;height:auto;padding:50px;margin-top:20px;margin-bottom:20px;border-top:1px solid #ccc;border-bottom:1px solid #ccc;">
-		<a href="#" class="btn btn-tab icon-meter">.btn .btn-tab</a>
-		<a href="#" class="btn btn-tab transparent icon-meter">.btn .btn-tab .transparent</a>
+		<a href="#" class="btn btn-tab icon-group">.btn .btn-tab</a>
+		<a href="#" class="btn btn-tab transparent icon-pulse">.btn .btn-tab .transparent</a>
 		<a href="#" class="btn btn-tab secondary icon-meter">.btn .btn-tab .secondary</a>
-		<a href="#" class="btn btn-tab secondary-transparent icon-meter">.btn .btn-tab .secondary-transparent</a>
+		<a href="#" class="btn btn-tab secondary-transparent icon-piechart">.btn .btn-tab .secondary-transparent</a>
 	</div>
 
 	<div id="sidebar-default" class="sidebar collapsable collapsed push-<?php sidebar_position_class(); ?>">
