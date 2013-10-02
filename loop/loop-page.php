f1a499e34dc3a3ab9f1d4260c15f232788b721d4
bryanmonzon <monzonb@gmail.com>
2013-10-01 20:36:54 -0700

    removed a title

--- loop/loop-page.php
+++ loop/loop-page.php
@@ -1,9 +1,9 @@
 <article class="post post-<?php echo get_the_ID(); ?>">
-  <header>
+  <!-- <header>
     <h1 class="post-title">
-      <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+      <a href="<?php //the_permalink(); ?>"><?php // the_title(); ?></a>
     </h1>
-  </header>
+  </header> -->
   <div class="content">
     <?php the_content(); ?>
   </div>
