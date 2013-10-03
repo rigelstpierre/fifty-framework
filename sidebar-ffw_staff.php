1c5761aa6cdfc3f520b2d63d1b6a1ce1be2b3ffb
bryanmonzon <monzonb@gmail.com>
2013-10-03 16:26:51 -0700

    retest

--- sidebar-ffw_staff.php
+++ sidebar-ffw_staff.php
@@ -3,18 +3,18 @@
  * The sidebar containing the secondary widget area, displays on posts and pages.
  * If no active widgets in this sidebar, it will be hidden completely.
 */
-$staff_label   		= strtolower(ffw_staff_get_label_singular() );
+$staff_label = strtolower(ffw_staff_get_label_singular() );
 
-if ( ! is_active_sidebar( $staff_label . '_default' ) ) {
+if ( ! is_active_sidebar( 'staff_default' ) ) {
   return;
 }
 ?>
 	
-<?php if ( is_active_sidebar( $staff_label . '_default' ) ) : ?>
+<?php if ( is_active_sidebar( 'staff_default' ) ) : ?>
 	<div class="sidebar-inner">
 		<div class="widget-area">
 			<h1><?php echo $staff_label; ?></h1>
-			<?php dynamic_sidebar( $staff_label . '_default' ); ?>
+			<?php dynamic_sidebar( 'staff_default' ); ?>
 		</div><!-- .widget-area -->
 	</div><!-- .sidebar-inner -->
 
