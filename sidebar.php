a1761c1fa0682874129442f647cd318d32a9a2c1
bryanmonzon <monzonb@gmail.com>
2013-10-03 14:44:57 -0700

    sidebars galore

--- sidebar.php
+++ sidebar.php
@@ -12,12 +12,38 @@ if ( ! is_active_sidebar( 'sidebar_default' ) && ! is_active_sidebar( 'sidebar_f
 ================================================== */ 
 if ( is_home() || is_front_page() || is_page() || is_single() || is_archive() ): ?>
 
-  <?php if ( is_active_sidebar( 'sidebar_default' ) ) : ?>
-  	<div class="sidebar-inner">
-  		<div class="widget-area">
-  			<?php dynamic_sidebar( 'sidebar_default' ); ?>
-  		</div><!-- .widget-area -->
-  	</div><!-- .sidebar-inner -->
-  <?php endif; ?>
+	<?php if ( is_active_sidebar( 'sidebar_default' ) ) : ?>
+		<div class="sidebar-inner">
+			<div class="widget-area">
+				<?php dynamic_sidebar( 'sidebar_default' ); ?>
+			</div><!-- .widget-area -->
+		</div><!-- .sidebar-inner -->
+	<?php endif; ?>
+
+<?php elseif( is_singular( 'ffw_staff') ) : ?>
+	
+	<?php $portfolio_label = strtolower(ffw_port_get_label_singular() ); ?>
+	<?php if ( is_active_sidebar( $portfolio_label . '_default' ) ) : ?>
+		
+		<div class="sidebar-inner">
+			<div class="widget-area">
+				<?php dynamic_sidebar( $portfolio_label . '_default' ); ?>
+			</div><!-- .widget-area -->
+		</div><!-- .sidebar-inner -->
+
+	<?php endif; ?>
+
+<?php elseif( is_singular( 'ffw_portfolio' ) ) : ?>
+	
+	<?php $staff_label = strtolower(ffw_staff_get_label_singular() ); ?>
+	<?php if ( is_active_sidebar( $staff_label. '_default' ) ) : ?>
+			
+		<div class="sidebar-inner">
+			<div class="widget-area">
+				<?php dynamic_sidebar( $staff_label . '_default' ); ?>
+			</div><!-- .widget-area -->
+		</div><!-- .sidebar-inner -->
+
+	<?php endif; ?>
 
 <?php endif; ?>
