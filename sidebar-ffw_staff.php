9b8f7232aad9a479f780483ac1886235f760f101
bryanmonzon <monzonb@gmail.com>
2013-10-03 16:23:49 -0700

    sidebar staff edit

--- sidebar-ffw_staff.php
+++ sidebar-ffw_staff.php
@@ -3,7 +3,6 @@
  * The sidebar containing the secondary widget area, displays on posts and pages.
  * If no active widgets in this sidebar, it will be hidden completely.
 */
-$portfolio_label    = strtolower(ffw_port_get_label_singular() );
 $staff_label   		= strtolower(ffw_staff_get_label_singular() );
 
 if ( ! is_active_sidebar( $staff_label . '_default' ) ) {
@@ -14,6 +13,7 @@ if ( ! is_active_sidebar( $staff_label . '_default' ) ) {
 <?php if ( is_active_sidebar( $staff_label . '_default' ) ) : ?>
 	<div class="sidebar-inner">
 		<div class="widget-area">
+			<h1><?php echo $staff_label; ?></h1>
 			<?php dynamic_sidebar( $staff_label . '_default' ); ?>
 		</div><!-- .widget-area -->
 	</div><!-- .sidebar-inner -->
