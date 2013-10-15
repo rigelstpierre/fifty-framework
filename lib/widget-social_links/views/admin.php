3c707a09c765d4929731f95a681089221246504e
Alexander Zizzo <elzi@nerdi.net>
2013-10-15 00:07:53 -0700

    fix dropbox conflicts

--- lib/widget-social_links/views/admin.php
+++ lib/widget-social_links/views/admin.php
@@ -19,5 +19,32 @@
         <label for="<?php echo $this->get_field_id( 'social_link_twitter' ); ?>"><?php _e( 'Twitter URL:', 'FFW' ); ?></label>
         <input id="<?php echo $this->get_field_id( 'social_link_twitter' ); ?>" name="<?php echo $this->get_field_name( 'social_link_twitter' ); ?>" type="text" value="<?php echo $social_link_twitter; ?>"/>
     </div>
+
+    <div class="social_link_linkedin option" style="margin-top:10px;">
+        <label for="<?php echo $this->get_field_id( 'social_link_linkedin' ); ?>"><?php _e( 'LinkedIn URL:', 'FFW' ); ?></label>
+        <input id="<?php echo $this->get_field_id( 'social_link_linkedin' ); ?>" name="<?php echo $this->get_field_name( 'social_link_linkedin' ); ?>" type="text" value="<?php echo $social_link_linkedin; ?>"/>
+    </div>
+
+    <div class="social_link_dribbble option" style="margin-top:10px;">
+        <label for="<?php echo $this->get_field_id( 'social_link_dribbble' ); ?>"><?php _e( 'Dribbble URL:', 'FFW' ); ?></label>
+        <input id="<?php echo $this->get_field_id( 'social_link_dribbble' ); ?>" name="<?php echo $this->get_field_name( 'social_link_dribbble' ); ?>" type="text" value="<?php echo $social_link_dribbble; ?>"/>
+    </div>
+
+    <div class="social_link_flickr option" style="margin-top:10px;">
+        <label for="<?php echo $this->get_field_id( 'social_link_flickr' ); ?>"><?php _e( 'Flickr URL:', 'FFW' ); ?></label>
+        <input id="<?php echo $this->get_field_id( 'social_link_flickr' ); ?>" name="<?php echo $this->get_field_name( 'social_link_flickr' ); ?>" type="text" value="<?php echo $social_link_flickr; ?>"/>
+    </div>
+
+    <div class="social_link_youtube option" style="margin-top:10px;">
+        <label for="<?php echo $this->get_field_id( 'social_link_youtube' ); ?>"><?php _e( 'YouTube URL:', 'FFW' ); ?></label>
+        <input id="<?php echo $this->get_field_id( 'social_link_youtube' ); ?>" name="<?php echo $this->get_field_name( 'social_link_youtube' ); ?>" type="text" value="<?php echo $social_link_youtube; ?>"/>
+    </div>
+
+    <div class="social_link_vimeo option" style="margin-top:10px;">
+        <label for="<?php echo $this->get_field_id( 'social_link_vimeo' ); ?>"><?php _e( 'Vimeo URL:', 'FFW' ); ?></label>
+        <input id="<?php echo $this->get_field_id( 'social_link_vimeo' ); ?>" name="<?php echo $this->get_field_name( 'social_link_vimeo' ); ?>" type="text" value="<?php echo $social_link_vimeo; ?>"/>
+    </div>
+
     
-</div><!-- /.standard-personal-image-wrapper -->
+    
+</div><!-- /.standard-author-info-wrapper -->
