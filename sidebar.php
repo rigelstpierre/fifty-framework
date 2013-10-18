991cf390f3a472fcdb2f2e7e9492ef1a30af4ed5
bryanmonzon <monzonb@gmail.com>
2013-10-18 14:45:22 -0700

    is_search addition

--- sidebar.php
+++ sidebar.php
@@ -10,7 +10,7 @@ if ( ! is_active_sidebar( 'sidebar_default' ) && ! is_active_sidebar( 'sidebar_f
 
 <?php /* DEFAULT
 ================================================== */ 
-if ( is_home() || is_front_page() || is_page() || is_single() || is_archive() ): ?>
+if ( is_home() || is_front_page() || is_page() || is_single() || is_archive() || is_search() ): ?>
 
   <?php if ( is_active_sidebar( 'sidebar_default' ) ) : ?>
   	<div class="sidebar-inner">
